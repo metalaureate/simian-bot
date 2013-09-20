3b4447622fa5fde7aeaae42fc308d3345c93d57e
Simon Hill <simon@infoqual.net>
2013-09-20 12:51:00 -0700

    fix

--- scripts/ira-bot.js
+++ scripts/ira-bot.js
@@ -3,13 +3,13 @@ var eliza = new ira();
 
 module.exports = function(robot) {
 
-    return robot.respond(/simian:(.*)/i, function(msg) {
+    return robot.respond(/(.*)/i, function(msg) {
        console.log(msg.match) ;
 
        var user_input=msg.match[1];
        var bot_output=eliza.transform(user_input) ;
         console.log(bot_output) ;
-       return msg.send(bot_output)
+       return; // msg.send(bot_output)
     });
 };
 
