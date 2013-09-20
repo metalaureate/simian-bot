821b237a4254adbd5c1da2d62d553a68224dfe75
Simon Hill <simon@infoqual.net>
2013-09-20 12:18:46 -0700

    fix

--- scripts/ira-bot.js
+++ scripts/ira-bot.js
@@ -1,6 +1,6 @@
 module.exports = function(robot) {
     return robot.respond(/(.*)/i, function(msg) {
-        console.log(msg);
-       return msg.send(msg)
+
+       return msg.send('you talking to me?')
     });
 };
