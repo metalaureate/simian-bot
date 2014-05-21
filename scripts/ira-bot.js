6e0976f98658f4349409676fba5bd5238e4209d6
metalaureate <Holonarchy0>
2014-05-21 13:45:45 -0700

    spam detection

--- scripts/ira-bot.js
+++ scripts/ira-bot.js
@@ -1,17 +1,22 @@
 var ira = require('../ira.js');
 var eliza = new ira();
-var spam = require('../spam.js')
+var spam = require('../spam.js');
+var util = require('util');
+
 module.exports = function (robot) {
 
     return robot.respond(/(.*)/i, function (msg) {
 
 
-        console.log(msg.match);
+        //console.log(msg.match);
 
         var user_input = msg.match[1];
+        console.log(user_input);
         var bot_output = eliza.transform(user_input);
-        spam.predict('demo1', user_input, function (spam_prediction) {
-            console.log(spam_prediction);
+        spam.predict('demo1', [user_input], function (spam_prediction) {
+            console.log(util.inspect(spam_prediction, { showHidden: true, depth: null }));
+
+            msg.send(bot_output)
         });
 
 
