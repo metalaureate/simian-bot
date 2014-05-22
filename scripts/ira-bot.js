2ab42fb22a97c55abeb7e6b24ca067aae91d1866
metalaureate <Holonarchy0>
2014-05-22 13:22:54 -0700

    styling

--- scripts/ira-bot.js
+++ scripts/ira-bot.js
@@ -30,7 +30,7 @@ module.exports = function (robot) {
                 }
             });
             if (spam_score>.6) {bot_output="I'm not answering that, you creep!";}
-            meter=Math.round(((((1-spam_score)-0.5)*2)-1),2);
+            meter=Math.round((spam_score-0.5)*10*2,2);
             msg.send('METER READING '+meter+' (-1 to +1) \n\n'+bot_output);
         });
 
